4989e1c1aee5c164000396798957b45b0c8f21e5
Daviti <datoqajaia10@gmail.com>
2026-01-06 20:50:42 +0400

    clear project

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -5,13 +5,8 @@ import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import com.example.project.ui.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +17,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         cont = findViewById(R.id.cont);
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.cont, new LoginFragment()).commit();
     }
     public void ChangeFragment(Fragment fm){
         getSupportFragmentManager().beginTransaction().replace(R.id.cont, fm).commit();
