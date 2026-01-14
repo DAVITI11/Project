e4c13a5e05c7d90ebd173d3b28300faa2a4c4067
Daviti <datoqajaia10@gmail.com>
2026-01-15 00:56:50 +0400

    10

--- app/src/main/java/com/example/project/Fragment_Sell.java
+++ app/src/main/java/com/example/project/Fragment_Sell.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment_Sell extends Fragment {
+    ImageView BtnHm;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,5 +22,9 @@ public class Fragment_Sell extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        BtnHm = view.findViewById(R.id.btnHomeTop);
+        BtnHm.setOnClickListener(v -> {
+            ((MainActivity)getActivity()).ChangeFragment(new Fragment_Home());
+        });
     }
 }
